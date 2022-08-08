73336c5b28bc3ec7ac08c877e978a0ebe9197c2a
Fúlvio <fulviocanducci@hotmail.com>
2022-08-08 17:46:11 -0300

    sources

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link href="/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="/css/app.base.css" rel="stylesheet" />
     <livewire:styles />
 </head>
 <body>
@@ -119,8 +120,8 @@ oi
         </div>
       </div>
     <livewire:scripts />
-    <script src="/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="/js/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
+    <script src="/js/bootstrap.bundle.min.js"></script>
+    <script src="/js/feather.min.js"></script>
     <script src="/js/dashboard.js"></script>
 </body>
 </html>
